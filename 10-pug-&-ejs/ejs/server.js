49ccddc2d1dd496f2e639175aa07672cf71e692b
ddelavega <diegodelavega08@gmail.com>
2022-06-30 04:10:37 -0300

    5-motores-de-plantillas - 10-motores-de-plantillas

---
+++ 10-pug-&-ejs/ejs/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const routerProductos = require('./../api/routerProductos');
+require('dotenv').config();
+
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+
+
+const port = process.env.PORT || 8081;
+
+app.use('/', routerProductos);
+
+// app.get("/", (req, res) => {
+//   res.render('index');
+// });
+
+const server = app.listen(port, () => {
+  console.log(`Escuchando al puerto: ${port} en http://localhost:${port}`);
+});
+server.on('error', error => console.log(`Error en servidor${error}`));
+
+module.exports = server;
